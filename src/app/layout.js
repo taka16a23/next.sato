6a1fe621a01db6fcf136ef73fc9a1095d90172c4
a23 <takahiroatsumi0517@gmail.com>
2025-02-18 05:54:01 +0900

    Add assets and layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,22 @@
 import "./globals.css";
+import header_img from '@/assets/images/header.webp';
 
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body>
-        <p>root header</p>
-        <main>
-          {children}
-        </main>
+        <div id="root">
+          <div className="wrapper">
+            <div className="wrapper-inner">
+              <div id="content">
+                <div className="content-inner">
+                  {children}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </body>
     </html>
   );
