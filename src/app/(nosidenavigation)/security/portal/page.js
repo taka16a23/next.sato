449ba2930e931af353143db460097cd501209ee1
a23 <takahiroatsumi0517@gmail.com>
2025-02-23 20:45:28 +0900

    Add security portal page

--- src/app/(nosidenavigation)/security/portal/page.js
+++ src/app/(nosidenavigation)/security/portal/page.js
@@ -1,7 +1,131 @@
-export default function Page() {
-  return (
-    <div>
-      <p>security portal</p>
-    </div>
-  );
+"use client"
+import React, { Component } from "react";
+
+import axios from "axios";
+import moment from "moment";
+import { Timeline } from 'react-twitter-widgets'
+import ContentLoader from "react-content-loader"
+
+import AliasRoutes from "@/routes/AliasRoutes";
+import Emergency from '@/components/Emergency'
+import WeatherAlert from '@/components/WeatherAlert'
+
+
+export default class Page extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoadedTimeline: false,
+      serverDateTime: null,
+    }
+  }
+
+  componentDidMount() {
+    // サーバー時刻を利用
+    let serverURL = process.env.REACT_APP_URL + AliasRoutes.Favicon;
+    axios.head(serverURL).then(res => {
+      let serverDateTime = new Date(res.headers.date);
+      // リンク切れ対策
+      // 30秒
+      let fixSeconds = 30000;
+      serverDateTime = new Date(serverDateTime - fixSeconds);
+      // 5分で丸め
+      let minutes = serverDateTime.getMinutes();
+      minutes = Math.floor(minutes / 5) * 5;
+      let fixDateTime = new Date(serverDateTime.getFullYear(),
+                                 serverDateTime.getMonth(), serverDateTime.getDate(),
+                                 serverDateTime.getHours(), minutes);
+      let riverDateTime = moment(fixDateTime);
+      this.setState({
+        riverDateTime: riverDateTime,
+      });
+    });
+  }
+
+  onLoadTimeline() {
+    this.setState({
+      isLoadedTimeline: true,
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="main-area">
+          <main id="main">
+            <div className="main-item">
+              <h2 className="main-title">
+                <span className="title">避難情報</span>
+              </h2>
+              <div className="main-body">
+                <div className="emergency-list">
+                  <div className="emergency-item emergencyInfoWrapper">
+                    <Emergency/>
+                  </div>
+                  <div className="emergency-item security-timeline">
+                    <ContentLoader
+                       speed={2}
+                       width="100%"
+                       height={400}
+                       backgroundColor="#f3f3f3"
+                       foregroundColor="#ecebeb"
+                       style={{display: this.state.isLoadedTimeline !== true ? "block" : "none"}}
+                    >
+                      <rect x="0" y="0" rx="0" ry="0" width="100%" height="400"/>
+                    </ContentLoader>
+                    <Timeline
+                      style={{display: this.state.isLoadedTimeline !== true ? "none" : "block"}}
+                      dataSource={{
+                        sourceType: 'profile',
+                        screenName: 'otsu_kikibousai'
+                      }}
+                      options={{
+                        lang: "ja",
+                        height: '400'
+                      }}
+                      onLoad={this.onLoadTimeline.bind(this)}
+                    />
+                    <div>※表示にはX(twitter)のログインが必要です</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="main-item">
+              <h2 className="main-title">
+                <a className="link" href="http://shiga-bousai.jp/announce/weather.php" alt="気象特別警報・警報・注意報" target="_blank" title="気象特別警報・警報・注意報" rel="noreferrer" >
+                  <span className="title">気象特別警報・警報・注意報</span>
+                  <i className="mdi mdi-open-in-new"></i>
+                </a>
+              </h2>
+              <WeatherAlert/>
+            </div>
+            <div className="main-item">
+              <h2 className="main-title">
+                <a className="link" href="http://c.shiga-bousai.jp/shigapref/pc/camDetail_6191FF05.html" alt="大戸川(石居橋)の様子" target="_blank" title="大戸川(石居橋)の様子" rel="noreferrer" >
+                  <span className="title">大戸川(石居橋)の様子</span>
+                  <i className="mdi mdi-open-in-new"></i>
+                </a>
+              </h2>
+              <h3>
+                <a href="http://c.shiga-bousai.jp/shigapref/pc/camDetail_6191FF05.html" target="_blank" title="滋賀県河川防災カメラのページへ移動" rel="noreferrer" >滋賀県 河川防災カメラ {this.state.riverDateTime === undefined ? "" : this.state.riverDateTime.format('YYYY年M月D日 HH:mm')} 現在</a>
+              </h3>
+              <div className="rivercam-container">
+                <div className="riverAlertEntry">
+                  <a href="http://c.shiga-bousai.jp/shigapref/pc/camDetail_6191FF05.html" target="_blank" title="滋賀県河川防災カメラ" rel="noreferrer" >
+                    <img src={this.state.riverDateTime === undefined ? "" : "/shigapref/pc/cameraData/images/6191FF05/" + this.state.riverDateTime.format('YYYYMMDDHHmm') + ".jpg"} alt="石居橋の現在の様子" width="640" height="480"/>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div id="evacuation-map" className="main-item">
+              <div className="evacuationMapArea">
+                <iframe title="避難地図" src="https://www.google.com/maps/d/embed?mid=zJXaXK9GQ-AU.kwVpZ6T_kTts" sandbox='allow-scripts allow-modal' loading='lazy'></iframe>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+  };
 }
---
+++ src/components/Emergency.js
@@ -0,0 +1,87 @@
+"use client"
+import React, { Component } from "react";
+
+import { ServiceFactory } from '@/services';
+
+
+export default class Emergency extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      model: null,
+    }
+  }
+
+  componentDidMount() {
+    // 緊急情報を取得
+    let securityService = ServiceFactory.createSecurityService();
+    securityService.getEmergency().then(model => {
+      this.setState({
+        model: model,
+      });
+    }).catch(err => {
+      alert(err);
+    });
+  }
+
+  render() {
+    return (
+      <div className="emergencyInfoBox">
+        <a href="https://dis-shiga.jp/pc/topdis-shiga.html" target="_blank" rel="noreferrer">
+          <img className="title_pic" src="https://dis-shiga.jp/image/title.jpg" alt="dis-shiga" width="370" height="85"/>
+        </a>
+        <div className="emergencyInfoBoxHead">
+          <img className="icon" src="https://dis-shiga.jp/image/exclamation2.png" alt="exclamation2" width="32" height="32"/>
+          <span className="title">緊急情報</span>
+        </div>
+        <div>
+          <h2>
+            <span className="name">本部体制設置</span>
+            {
+              this.state.model === null || this.state.model.honbutaisei !== true ?
+                <span className="state noAnnouncement">発表なし</span> :
+                <span className="state hasAnnounced">発表あり</span>
+            }
+            <button onClick={()=> window.open("https://dis-shiga.jp/pc/hq/info.html", "_blank", 'noopener,noreferrer')}>&gt;　詳細</button>
+          </h2>
+        </div>
+        <div>
+          <h2>
+            <span className="name">避難情報
+              <a href="/other/避難情報.pdf" target="_blank" rel="noreferror">（避難情報とは）</a>
+            </span>
+            {
+              this.state.model === null || this.state.model.hinanjouhou !== true ?
+                <span className="state noAnnouncement">発令なし</span> :
+                <span className="state hasAnnounced">発令あり</span>
+            }
+            <button onClick={()=> window.open("https://dis-shiga.jp/pc/evacuation/info.html", "_blank", 'noopener,noreferrer')}>&gt;　詳細</button>
+          </h2>
+        </div>
+        <div>
+          <h2>
+            <span className="name">避難所情報</span>
+            {
+              this.state.model === null || this.state.model.hinanjojouhou !== true ?
+                <span className="state noAnnouncement">発表なし</span> :
+                <span className="state hasAnnounced">発表あり</span>
+            }
+            <button onClick={()=> window.open("https://dis-shiga.jp/pc/shelter/info.html", "_blank", 'noopener,noreferrer')}>&gt;　詳細</button>
+          </h2>
+        </div>
+        <div>
+          <h2>
+            <span className="name">国民保護情報</span>
+            {
+              this.state.model === null || this.state.model.kokuminhogojouhou !== true ?
+                <span className="state noAnnouncement">発表なし</span> :
+                <span className="state hasAnnounced">発表あり</span>
+            }
+            <button onClick={()=> window.open("https://dis-shiga.jp/pc/civil_protection/info.html", "_blank", 'noopener,noreferrer')}>&gt;　詳細</button>
+          </h2>
+        </div>
+      </div>
+    );
+  };
+}
