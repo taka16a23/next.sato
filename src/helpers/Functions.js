d152f9b7c75f72184c41c21e4556c9d8aeec84eb
a23 <takahiroatsumi0517@gmail.com>
2025-10-03 04:44:42 +0900

    Build for server side

--- src/helpers/Functions.js
+++ src/helpers/Functions.js
@@ -1,6 +1,9 @@
 export function focusInput(a_sName) {
-  var arrInputs = document.querySelectorAll('input[name="' + a_sName + '"]');
-  if(arrInputs) {
-    arrInputs[arrInputs.length -1].focus();
-  };
+  // document exists only browser
+  if (process.browser) {
+    var arrInputs = document.querySelectorAll('input[name="' + a_sName + '"]');
+    if(arrInputs) {
+      arrInputs[arrInputs.length -1].focus();
+    };
+  }
 }
